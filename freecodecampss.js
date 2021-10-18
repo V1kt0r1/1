e1efeed47b8d8aab3fc2a428dc9722400640e148
V1kt0r1 <92546662+V1kt0r1@users.noreply.github.com>
2021-10-18 20:58:01 +0300

    немного заданий

--- freecodecampss.js
+++ freecodecampss.js
@@ -1,1 +1,24 @@
-<script> hello</script>
+var a;
+a=7;
+
+var a;
+a = 7;
+var b;
+b=a;
+
+var a=9;
+
+var a=5;
+var b=10;
+var c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar= 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
