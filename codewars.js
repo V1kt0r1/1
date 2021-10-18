6b01a37b65c2569abb07e04fd29028dd39ee6b58
V1kt0r1 <92546662+V1kt0r1@users.noreply.github.com>
2021-10-18 21:41:20 +0300

    1

--- codewars.js
+++ codewars.js
@@ -2,4 +2,8 @@ function distinct(a) {
     return [...new Set(a)];
   }
 
-  
+  function rentalCarCost(d) {
+    const perday=40;
+      const skidka = d >=7 ? 50:d >=3?20:0;
+      return d * perday - skidka;
+    }
